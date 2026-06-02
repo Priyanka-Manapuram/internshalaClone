3ded3905e68ec87eeecf23b9d1a80a78635e2de7
PriyankaManapuram <priyankamanapuram08@gmail.com>
2026-06-02 15:21:15 +0530

    first commit

---
+++ backend/Routes/admin.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const adminuser = "admin";
+const adminpass = "admin";
+
+router.post("/adminlogin", (req, res) => {
+  const { username, password } = req.body;
+  if (username === adminuser && password === adminpass) {
+    return res.status(200).json({
+      success: true,
+      message: "Admin logged in",
+    });
+  }
+  return res.status(401).json({
+    success: false,
+    message: "Unauthorized",
+  });
+});
+module.exports = router;
+
